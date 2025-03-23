28325afdc497407c56899c0ec7ef5415eb59ff7b
tnieyu <tniestudyacc176@gmail.com>
2025-03-23 16:21:12 +0700

    feat: springmvc-init

---
+++ MySpringMVCV1/src/main/java/com/tnieyu/services/impls/CategoryServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tnieyu.services.impls;
+
+import com.tnieyu.pojo.Category;
+import com.tnieyu.repositories.CategoryRepository;
+import com.tnieyu.services.CategoryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    @Autowired
+    private CategoryRepository cateRepo;
+    
+    @Override
+    public List<Category> getCates() {
+        return cateRepo.getCates();
+    }
+    
+}
